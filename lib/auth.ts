10d87bca722a2d7ef2958d323ea68f5d74c79eb6
Flavio Aquino <bregarulai@gmail.com>
2026-05-27 15:37:51 -0400

    feat: Auth Credentials - email/password provider with registration

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,52 @@
 import { authConfig } from "./auth.config"
 import NextAuth from "next-auth"
+import bcrypt from "bcryptjs"
+import { prisma } from "@/lib/prisma"
+import Credentials from "next-auth/providers/credentials"
 
-export const { handlers, auth, signIn, signOut } = NextAuth(authConfig)
+const overrideProviders = {
+  ...authConfig.providers,
+  providers: [
+    ...authConfig.providers,
+    Credentials({
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        if (!credentials?.email || !credentials?.password) {
+          return null
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email as string },
+        })
+
+        if (!user || !user.password) {
+          return null
+        }
+
+        const isValid = await bcrypt.compare(
+          credentials.password as string,
+          user.password,
+        )
+
+        if (!isValid) {
+          return null
+        }
+
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          image: user.image,
+        }
+      },
+    }),
+  ],
+}
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  ...overrideProviders,
+})
